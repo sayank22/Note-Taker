b73e084bc68eedc3fbb78c75e723474b845c22c9
sayank22 <sayank10023@gmail.com>
2025-07-26 19:33:47 +0530

    basic folder setup complete

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,5 +6,9 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <h1 class="text-3xl font-bold underline text-amber-600">
+    Hello world!
+  </h1>
+
   </StrictMode>,
 )
